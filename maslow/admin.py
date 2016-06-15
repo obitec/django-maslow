807a497991a841e779d1b1d6cfd42edfd32dda1c
JR Minnaar <jr.minnaar@gmail.com>
2016-06-15 15:23:11 +0200

    Add more missing imports

--- maslow/admin.py
+++ maslow/admin.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib import admin
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
 from django.core.exceptions import FieldDoesNotExist
 from ckeditor.widgets import CKEditorWidget
 from django.contrib.auth.models import Permission
